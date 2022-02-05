a1d9fcc769ad0aab58586441be5fa821398dcb37
vibiksha <98578149+vibiksha@users.noreply.github.com>
2022-02-05 22:45:45 +0530

    Delete savedata.js

--- savedata.js
+++
@@ -1,5 +0,0 @@
-var username=document.getElementById("regusername");
-var email=document.getElementById("regemail");
-var password=document.getElementById("regpassword");
-var emailchk=document.getElementById("loginemail");
-var passwordchk=document.getElementById("loginpassword");
