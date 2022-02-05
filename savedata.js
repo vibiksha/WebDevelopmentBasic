771e9e182a3473d187751244fe75dd0fc09b90aa
vibiksha <98578149+vibiksha@users.noreply.github.com>
2022-02-05 22:48:50 +0530

    Add files via upload

---
+++ savedata.js
@@ -0,0 +1,5 @@
+var username=document.getElementById("regusername");
+var email=document.getElementById("regemail");
+var password=document.getElementById("regpassword");
+var emailchk=document.getElementById("loginemail");
+var passwordchk=document.getElementById("loginpassword");
